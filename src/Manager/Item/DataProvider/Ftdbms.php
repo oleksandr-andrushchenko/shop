c46120188509f34700c0cfdec7c6d78a5b7b1cae
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-06-07 17:36:42 +0300

    +duplicate method removed

--- src/Manager/Item/DataProvider/Ftdbms.php
+++ src/Manager/Item/DataProvider/Ftdbms.php
@@ -31,28 +31,4 @@ class Ftdbms extends DataProvider
     {
         // TODO: Implement getPricesByUri() method.
     }
-
-    public function getListByQuery(string $query, bool $prefix = false): array
-    {
-        $src = $this->manager->copy(true)
-            ->setStorage(Manager::STORAGE_FTDBMS)
-            ->addColumn('id')
-            ->addWhere(new Expr('MATCH(?)', ($prefix ? '' : '*') . $query . '*'));
-
-        if (strlen($query) <= 2) {
-            $src->setLimit(1000);
-        }
-
-        $ids = $src->getList('id');
-
-        if (is_array($ids)) {
-            if ($ids) {
-                return $this->manager->setStorage(Manager::STORAGE_RDBMS)
-                    ->addWhere([$this->manager->getEntity()->getPk() => $ids])
-                    ->getObjects();
-            }
-
-            return [];
-        }
-    }
 }
