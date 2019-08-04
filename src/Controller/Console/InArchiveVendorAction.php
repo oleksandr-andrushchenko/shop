af9f0869f43fd775acad088d7d9f00a5fd6efa2f
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-08-04 12:45:13 +0300

    +added

---
+++ src/Controller/Console/InArchiveVendorAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SNOWGIRL_SHOP\Controller\Console;
+
+use SNOWGIRL_CORE\Controller\Console\PrepareServicesTrait;
+use SNOWGIRL_SHOP\App\Console as App;
+
+class InArchiveVendorAction
+{
+    use PrepareServicesTrait;
+
+    public function __invoke(App $app)
+    {
+        $this->prepareServices($app);
+
+        if (!$vendorId = $app->request->get('param_1')) {
+            throw (new BadRequest)->setInvalidParam('vendor_id');
+        }
+
+        if (!$vendor = $app->managers->vendors->find($vendorId)) {
+            throw (new NotFound)->setNonExisting('vendor');
+        }
+
+        $aff1 = $app->managers->vendors->updateOne($vendor->setIsActive(false));
+        $aff2 = $app->managers->sources->updateMany(['is_cron' => 0], ['vendor_id' => $vendor->getId()]);
+        $aff3 = $app->utils->items->doInArchiveTransfer(['vendor_id' => $vendor->getId()]);
+
+        $app->response->setBody(implode("\r\n", [
+            __CLASS__,
+            'Vendor activate off: ' . var_export($aff1, true),
+            'Sources cron off: ' . var_export($aff2, true),
+            'Affected: ' . var_export($aff3, true)
+        ]));
+    }
+}
---
+++ src/Controller/Console/OutArchiveVendorAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SNOWGIRL_SHOP\Controller\Console;
+
+use SNOWGIRL_CORE\Controller\Console\PrepareServicesTrait;
+use SNOWGIRL_SHOP\App\Console as App;
+
+class OutArchiveVendorAction
+{
+    use PrepareServicesTrait;
+
+    public function __invoke(App $app)
+    {
+        $this->prepareServices($app);
+
+        if (!$vendorId = $app->request->get('param_1')) {
+            throw (new BadRequest)->setInvalidParam('vendor_id');
+        }
+
+        if (!$vendor = $app->managers->vendors->find($vendorId)) {
+            throw (new NotFound)->setNonExisting('vendor');
+        }
+
+        $aff1 = $app->managers->vendors->updateOne($vendor->setIsActive(true));
+        $aff2 = $app->managers->sources->updateMany(['is_cron' => 1], ['vendor_id' => $vendor->getId()]);
+        $aff3 = $app->utils->items->doOutArchiveTransfer(['vendor_id' => $vendor->getId()]);
+
+        $app->response->setBody(implode("\r\n", [
+            __CLASS__,
+            'Vendor activate on: ' . var_export($aff1, true),
+            'Sources cron on: ' . var_export($aff2, true),
+            'Affected: ' . var_export($aff3, true)
+        ]));
+    }
+}
