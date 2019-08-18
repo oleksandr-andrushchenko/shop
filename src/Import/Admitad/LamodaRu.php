2d05aebe46c0d35974e009b22563c22708e07bcf
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-08-19 02:47:18 +0300

    +added

---
+++ src/Import/Admitad/LamodaRu.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SNOWGIRL_SHOP\Import\Admitad;
+
+use SNOWGIRL_SHOP\Import\Admitad;
+
+class LamodaRu extends Admitad
+{
+    protected $filters = [
+        'categoryId' => [
+            'equal' => ['женщ'],
+            'not_equal' => ['муж', 'дево', 'мальч', 'детс']
+        ],
+        'currencyId' => [
+            'equal' => ['RUB']
+        ]
+    ];
+
+    protected $defaultAllowModifyOnly = false;
+
+    /**
+     * @param $row
+     *
+     * @return int|null
+     */
+    protected function getCategoryByRow($row)
+    {
+        $pk = 'category_id';
+        $map = $this->mappings[$pk];
+        $value = trim($row[$this->indexes[$this->mappings[$pk]['column']]]);
+
+        if (array_key_exists('modify', $map) && array_key_exists($value, $modifies = $map['modify']) && $modifies[$value]['value']) {
+            $this->tags = array_merge($this->tags, $modifies[$value]['tags']);
+            $this->sport = $this->sport || in_array('is_sport', $modifies[$value]);
+            $this->sizePlus = $this->sizePlus || in_array('is_size_plus', $modifies[$value]);
+            return (int)$modifies[$value]['value'];
+        }
+
+        $exp = explode('/', $value);
+
+        foreach (array_reverse($exp) as $peace) {
+            if (isset($this->sva[$pk]['nameToId'][$peace])) {
+                return $this->sva[$pk]['nameToId'][$peace];
+            }
+
+            foreach ($this->sva[$pk]['nameToId'] as $categoryName => $categoryId) {
+                if (false !== mb_stripos($peace, $categoryName)) {
+                    return $categoryId;
+                }
+            }
+        }
+
+        return parent::getCategoryByRow($row);
+    }
+}
