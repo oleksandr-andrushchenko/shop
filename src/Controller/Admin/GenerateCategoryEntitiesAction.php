ec6b7ad4dd0195af57c0cce2f37568c1dd56b8b4
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-09-25 20:13:14 +0300

    +added

---
+++ src/Controller/Admin/GenerateCategoryEntitiesAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SNOWGIRL_SHOP\Controller\Admin;
+
+use SNOWGIRL_SHOP\App\Web as App;
+use SNOWGIRL_CORE\Controller\Admin\PrepareServicesTrait;
+use SNOWGIRL_SHOP\RBAC;
+
+class GenerateCategoryEntitiesAction
+{
+    use PrepareServicesTrait;
+
+    public function __invoke(App $app)
+    {
+        $this->prepareServices($app);
+
+        $app->rbac->checkPerm(RBAC::PERM_ALL);
+
+        $app->managers->categoriesToEntities->generate();
+
+        $app->request->redirectBack();
+    }
+}
