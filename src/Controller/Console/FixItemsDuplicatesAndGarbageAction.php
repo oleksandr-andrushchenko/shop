5ba59e18790184a63a813967c4d84504815de4ee
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-12-16 01:05:21 +0200

    +process-duplicates and process-garbage methods added +improved

--- src/Controller/Console/FixItemsDuplicatesAndGarbageAction.php
+++ src/Controller/Console/FixItemsDuplicatesAndGarbageAction.php
@@ -31,48 +31,142 @@ class FixItemsDuplicatesAndGarbageAction
 
         $aff = 0;
 
+        $aff += $this->processDuplicates($app);
+        $aff += $this->processGarbage($app);
+
+        (new DeleteItemsNonExistingMvaAction)($app);
+
+        $app->response->addToBody(implode("\r\n", [
+            __CLASS__,
+            $app->response->setBody(is_int($aff) ? "DONE: {$aff}" : 'FAILED')
+        ]));
+    }
+
+    private function processDuplicates(App $app): int
+    {
+        $aff = 0;
+
+        $attrUtil = $app->utils->attrs;
+
         foreach ($app->managers->sources->getObjects() as $source) {
             (new WalkChunk(1000))
                 ->setFnGet(function ($page, $size) use ($app, $source) {
                     $pkQuotted = $app->storage->mysql->quote($app->managers->items->getEntity()->getPk());
-                    $partnerItemIdQuotted = $app->storage->mysql->quote('partner_item_id');
-                    $imageQuotted = $app->storage->mysql->quote('image');
-                    $cntQuotted = $app->storage->mysql->quote('cnt');
 
                     return $app->managers->items
                         ->setColumns([
-                            'category_id',
-                            'partner_link_hash',
                             new Expr('GROUP_CONCAT(' . $pkQuotted . ') AS ' . $pkQuotted),
-                            new Expr('GROUP_CONCAT(' . $partnerItemIdQuotted . ') AS ' . $partnerItemIdQuotted),
-                            new Expr('GROUP_CONCAT(' . $imageQuotted . ') AS ' . $imageQuotted),
-                            new Expr('COUNT(*) AS ' . $cntQuotted),
                         ])
                         ->setWhere([
-                            'import_source_id' => $source->getId()
+                            'import_source_id' => $source->getId(),
                         ])
                         ->setGroups([
                             'category_id',
-                            'partner_link_hash'
+                            'partner_link_hash',
                         ])
                         ->setHavings([
-                            new Expr($cntQuotted . ' > 1')
+                            new Expr('COUNT(*) > 1'),
                         ])
                         ->setOffset(($page - 1) * $size)
                         ->setLimit($size)
                         ->getArrays();
                 })
-                ->setFnDo(function ($rows) {
-//                    $pk = $app->managers->items->getEntity()->getPk();
-//
-//                    foreach ($rows as $row) {
-//                        $pkValues = explode(',', $row[$pk]);
-//                        # @todo...
-//                    }
+                ->setFnDo(function ($rows) use ($app, $attrUtil, &$aff) {
+                    /** @var App $app */
+                    $pk = $app->managers->items->getEntity()->getPk();
+
+                    foreach ($rows as $row) {
+                        $pkValues = explode(',', $row[$pk]);
+                        $pkToKeep = (int)min($pkValues);
+                        $pkValues = array_diff($pkValues, [$pkToKeep]);
+
+                        $insert = [];
+                        $delete = [$pk => []];
+
+                        foreach ($pkValues as $pkValue) {
+                            $pkValue = (int)$pkValue;
+
+                            $insert[] = [
+                                'id_from' => $pkValue,
+                                'id_to' => $pkToKeep
+                            ];
+
+                            $delete[$pk][] = $pkValue;
+                            $mva[$pkValue] = $pkToKeep;
+                        }
+
+                        $aff += $app->managers->itemRedirects->insertMany($insert);
+                        $aff += $app->managers->items->deleteMany($delete);
+                        $aff += $attrUtil->doTransferMvaValues($mva);
+                    }
                 })
                 ->run();
         }
 
-        $app->response->setBody(is_int($aff) ? "DONE: {$aff}" : 'FAILED');
+        return $aff;
+    }
+
+    private function processGarbage(App $app): int
+    {
+        $aff = 0;
+
+        $attrUtil = $app->utils->attrs;
+
+        foreach ($app->managers->sources->getObjects() as $source) {
+            (new WalkChunk(1000))
+                ->setFnGet(function ($page, $size) use ($app, $source) {
+                    $pkQuotted = $app->storage->mysql->quote($app->managers->items->getEntity()->getPk());
+
+                    return $app->managers->items
+                        ->setColumns([
+                            new Expr('GROUP_CONCAT(' . $pkQuotted . ') AS ' . $pkQuotted),
+                        ])
+                        ->setWhere([
+                            'import_source_id' => $source->getId(),
+                        ])
+                        ->setGroups([
+                            'category_id',
+                            'image',
+                        ])
+                        ->setHavings([
+                            new Expr('COUNT(*) > 1'),
+                        ])
+                        ->setOffset(($page - 1) * $size)
+                        ->setLimit($size)
+                        ->getArrays();
+                })
+                ->setFnDo(function ($rows) use ($app, $attrUtil, &$aff) {
+                    /** @var App $app */
+                    $pk = $app->managers->items->getEntity()->getPk();
+
+                    foreach ($rows as $row) {
+                        $pkValues = explode(',', $row[$pk]);
+                        $pkToKeep = (int)min($pkValues);
+                        $pkValues = array_diff($pkValues, [$pkToKeep]);
+
+                        $insert = [];
+                        $delete = [$pk => []];
+
+                        foreach ($pkValues as $pkValue) {
+                            $pkValue = (int)$pkValue;
+
+                            $insert[] = [
+                                'id_from' => $pkValue,
+                                'id_to' => $pkToKeep
+                            ];
+
+                            $delete[$pk][] = $pkValue;
+                            $mva[$pkValue] = $pkToKeep;
+                        }
+
+                        $aff += $app->managers->itemRedirects->insertMany($insert);
+                        $aff += $app->managers->items->deleteMany($delete);
+                        $aff += $attrUtil->doTransferMvaValues($mva);
+                    }
+                })
+                ->run();
+        }
+
+        return $aff;
     }
 }
