08de3088252f696400c8eea01853396e6c20ba8f
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-02-13 22:49:03 +0200

    +added

---
+++ src/Import/Admitad/SheinRu.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SNOWGIRL_SHOP\Import\Admitad;
+
+use SNOWGIRL_CORE\Helper\Arrays;
+use SNOWGIRL_SHOP\Import\Admitad;
+
+class SheinRu extends Admitad
+{
+    protected $colorIndex;
+    protected $sizesIndex;
+
+    protected function before()
+    {
+        $this->colorIndex = isset($this->indexes['color']);
+        $this->sizesIndex = isset($this->indexes['size']);
+    }
+
+    /**
+     * Returns mixed names
+     *
+     * @param $row
+     *
+     * @return array
+     */
+    protected function getColorsByRow($row)
+    {
+        if ($this->colorIndex && $color = trim($row[$this->indexes['color']])) {
+            return [$color];
+        }
+
+        return parent::getColorsByRow($row);
+    }
+
+    /**
+     * Returns mixed names
+     *
+     * @param $row
+     *
+     * @return array
+     */
+    protected function getSizesByRow($row)
+    {
+        if ($this->sizesIndex && $sizes = trim($row[$this->indexes['size']])) {
+            return array_map('trim', explode(' ', $sizes));
+        }
+
+        return parent::getSizesByRow($row);
+    }
+}
