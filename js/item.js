3bb4f68bfb3039322d8b851f512ed5f21e85c469
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-04-16 22:42:40 +0300

    +is in stock check implemented +soft non-func re-factoring

--- js/item.js
+++ js/item.js
@@ -2,7 +2,6 @@ var snowgirlApp = function (snowgirlCore) {
     this.core = snowgirlCore;
 
     this.initArgs();
-//    this.initDOM();
     this.initCallbacks();
 };
 snowgirlApp.prototype.initArgs = function () {
@@ -14,8 +13,11 @@ snowgirlApp.prototype.initArgs = function () {
     this.ajaxLoaderSelector = '.items-ajax-loader';
     this.ajaxLoaderButtonSelector = this.ajaxLoaderSelector + ' .btn';
     this.detailsSelector = '.details';
-//     this.btnCheckSelector = this.detailsSelector + ' .btn-check';
-    this.btnShowRelatedSelector = this.detailsSelector + ' .btn-related';
+    this.btnsSelector = this.detailsSelector + ' .item-buttons';
+    this.btnCheckSelector = this.btnsSelector + ' .btn-check';
+    this.btnRelatedSelector = this.btnsSelector + ' .btn-related';
+    this.btnDetailsSelector = this.btnsSelector + ' .btn-details';
+    this.btnBuySelector = this.btnsSelector + ' .btn-buy';
     this.relatedSelector = '.related-items';
 
     if (this.core.getConfig('typeOwn', false)) {
@@ -27,13 +29,8 @@ snowgirlApp.prototype.initArgs = function () {
         this.btnCartQuantityIncSelector = tmp + ' .btn-quantity-inc';
         this.btnCartAddSelector = this.typeOwnSelector + ' .item-cart-buttons .btn-add-to-cart';
     }
-
-//     this.core.addVisitedItem(this.core.getConfig('itemId'));
 };
 snowgirlApp.prototype.initCallbacks = function () {
-//     var img = $(this.easyZoomSelector).find('img')[0];
-
-//     if ((img.naturalHeight > img.height) || (img.naturalWidth > img.width)) {
     if (this.core.getConfig('deviceDesktop', false)) {
         $(this.easyZoomSelector).easyZoom({
             loadingNotice: 'Загружаю картинку...',
@@ -42,12 +39,8 @@ snowgirlApp.prototype.initCallbacks = function () {
         });
     }
 
-//         this.core.addWindowResizeCallback($.proxy(this.onWindowResize, this), true);
-//     }
-
     this.core.$document
-    //         .one('click', this.btnCheckSelector, $.proxy(this.onCheckButtonClick, this))
-        .on('click', this.btnShowRelatedSelector, $.proxy(this.onShowRelatedButtonClick, this))
+        .on('click', this.btnRelatedSelector, $.proxy(this.onShowRelatedButtonClick, this))
         .on('click', this.thumbnailsLinksSelector, $.proxy(this.onImageThumbnailClick, this))
         .on('click', this.ajaxLoaderButtonSelector, $.proxy(this.onAjaxLoaderClick, this));
 
@@ -56,13 +49,16 @@ snowgirlApp.prototype.initCallbacks = function () {
             .on('click', this.btnCartSizeSelector, $.proxy(this.onCartSizeButtonClick, this))
             .on('click', this.btnCartQuantityDecSelector, $.proxy(this.onCartQuantityDecButtonClick, this))
             .on('click', this.btnCartQuantityIncSelector, $.proxy(this.onCartQuantityIncButtonClick, this))
-            .on('click', this.btnCartAddSelector, $.proxy(this.onCartAddButtonClick, this))
-        ;
+            .on('click', this.btnCartAddSelector, $.proxy(this.onCartAddButtonClick, this));
 
         $(this.btnCartSizeSelector + ':first').trigger('click');
     }
 
-//     $(this.btnCheckSelector).trigger('click');
+    if (!this.core.getConfig('isArchive', false) &&
+        this.core.getConfig('isInStockCheck', false) &&
+        this.core.getConfig('isInStock', false)) {
+        this.checkIsInStock();
+    }
 };
 snowgirlApp.prototype.onCartSizeButtonClick = function (ev) {
     var $btn = $(ev.target).getButton();
@@ -112,113 +108,23 @@ snowgirlApp.prototype.onCartAddButtonClick = function (ev) {
     });
     var $btn = $(ev.target).getButton();
 
-//     this.core.addCartItem(this.getItemId(), this.getSize(), this.getQuantity(), function () {
-//
-//     });
-
     $btn.blur();
 };
-snowgirlApp.prototype.onCheckButtonClick = function (ev) {
-    var $btn = $(ev.target).getButton();
-
-    //@todo add loading state for image label also! (user shouldnt think that it is out of stock!)
-    $btn.toggleLoading('Проверяю');
-
-    var $label = $(this.imagesSelector + ' .item-out-of-stock');
-//    $label.toggleLoading('Проверяю');
-
-    var event = $.proxy(function (isInStock) {
-        this.core.gtag.sendEvent('item_check', {
-            _category: 'item is in stock',
-            _action: 'click',
-            _label: 'item',
-            item_id: this.itemId,
-            vendor_id: this.vendorId,
-            in_stock: isInStock,
-            value: null === isInStock ? 0 : !!isInStock
-        });
-    }, this);
-
-    var unknown = $.proxy(function () {
-        $(this.detailsSelector + ' .item-buttons')
-            .append($('<a/>', {
-                href: this.core.getUriByRoute('default', {
-                    action: 'buy',
-                    id: this.itemId,
-                    src: 'item_check'
-                }),
-                class: 'btn btn-lg btn-primary',
-                target: '_blank',
-                rel: 'nofollow'
-            })
-                .append($('<span/>', {class: 'fa fa-refresh'}))
-                .append(' ')
-                .append('Проверить на ' + this.core.getConfig('vendorName')));
-    }, this);
-
+snowgirlApp.prototype.checkIsInStock = function () {
     this.core.makeRequestByRoute('default', {action: 'check-item-is-in-stock', id: this.itemId})
         .then($.proxy(function (body) {
-            $btn.toggleLoading();
-
             if (!body.hasOwnProperty('in_stock')) {
                 body = {in_stock: null};
             }
 
-            if (true === body['in_stock']) {
-                $label.remove();
-                $(this.detailsSelector + ' .item-buttons')
-                    .append($('<a/>', {
-                        href: this.core.getUriByRoute('default', {
-                            action: 'buy',
-                            id: this.itemId,
-                            src: 'item_photos'
-                        }),
-                        class: 'btn btn-lg btn-info btn-photos',
-                        target: '_blank',
-                        rel: 'nofollow',
-                        text: 'Еще фото'
-                    }))
-                    .append(' ')
-                    .append($('<a/>', {
-                        href: this.core.getUriByRoute('default', {
-                            action: 'buy',
-                            id: this.itemId,
-                            src: 'item_buy'
-                        }),
-                        class: 'btn btn-lg btn-primary btn-buy',
-                        target: '_blank',
-                        rel: 'nofollow'
-                    })
-                        .append($('<span/>', {class: 'fa fa-shopping-cart'}))
-                        .append(' ')
-                        .append('Купить на ' + this.core.getConfig('vendorName')));
-                $btn.remove();
-                event(true);
-            } else if (false === body['in_stock']) {
-                $label.addClass('checked');
-                $(this.detailsSelector + ' .item-buttons')
-                    .append($('<span/>', {class: 'btn btn-lg btn-out-of-stock'}))
-                    .append($('<button/>', {
-                        type: 'button',
-                        class: 'btn btn-lg btn-primary btn-related'
-                    })
-                        .append($('<span/>', {class: 'fa fa-random'}))
-                        .append(' ')
-                        .append('Похожие'));
-                $btn.remove();
-                event(false);
-            } else {
-//                $label.toggleLoading();
-                unknown();
-                $btn.remove();
-                event(null);
+            if (false === body['in_stock']) {
+                $(this.btnsSelector)
+                    .prepend($('<span/>', {class: 'btn btn-lg btn-out-of-stock'}));
+                $(this.btnDetailsSelector).remove();
+                $(this.btnBuySelector).remove();
             }
         }, this))
         .catch(function () {
-//            $label.toggleLoading();
-            unknown();
-            $btn.remove();
-            event(null);
         });
 };
 snowgirlApp.prototype.onShowRelatedButtonClick = function () {
@@ -286,7 +192,6 @@ snowgirlApp.prototype.onAjaxLoaderClick = function (ev) {
                 var $window = $(window);
                 var tmp = $window.scrollTop();
                 $('.widget-grid-items .row').append(body.view);
-//                $window.scrollTop($ajaxLoader.offset().top - window.innerHeight + $ajaxLoader.outerHeight() + 10);
                 $window.scrollTop(tmp);
 
                 if (('isLastPage' in body) && body['isLastPage']) {
