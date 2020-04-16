3419c800ea0b36d968ac625382fda424743dc08d
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-04-16 22:49:45 +0300

    +improvements +re-factoring

--- src/Controller/Outer/CheckItemIsInStockAction.php
+++ src/Controller/Outer/CheckItemIsInStockAction.php
@@ -4,20 +4,28 @@ namespace SNOWGIRL_SHOP\Controller\Outer;
 
 use SNOWGIRL_CORE\Controller\Outer\PrepareServicesTrait;
 use SNOWGIRL_CORE\Http\Exception\BadRequestHttpException;
-use SNOWGIRL_CORE\Exception\HTTP\MethodNotAllowed;
+use SNOWGIRL_CORE\Http\Exception\MethodNotAllowedHttpException;
 use SNOWGIRL_CORE\Http\Exception\NotFoundHttpException;
+use SNOWGIRL_SHOP\Entity\Item;
 use SNOWGIRL_SHOP\Http\HttpApp as App;
 
 class CheckItemIsInStockAction
 {
     use PrepareServicesTrait;
 
+    const CACHE_KEY_CHECKED = 'item-is-in-stock-checked-%id%';
+    const CACHE_LIFETIME_CHECKED = 3600;
+
+    /**
+     * @param App $app
+     * @throws \SNOWGIRL_CORE\Exception
+     */
     public function __invoke(App $app)
     {
         $this->prepareServices($app);
 
         if (!$app->request->isGet()) {
-            throw (new MethodNotAllowed)->setValidMethod('get');
+            throw (new MethodNotAllowedHttpException)->setValidMethod('get');
         }
 
         if (!$id = $app->request->get('id')) {
@@ -29,26 +37,57 @@ class CheckItemIsInStockAction
         }
 
         if ($item->isInStock()) {
-            $answer = true;
-        } else {
-            $answer = $app->managers->items->checkRealIsInStock($item);
+            $checkedCacheKey = $this->getCheckedCacheKey($item);
+            $app->container->cache->has($checkedCacheKey, $checkedCacheValue);
 
-            if (true === $answer) {
-                $item->setIsInStock(true);
-                $app->managers->items->updateOne($item);
+            if ($checkedCacheValue) {
+                $answer = true;
+                $app->container->logger->info('Is in stock item already checked', [
+                    'item_id' => $item->getId(),
+                ]);
+            } else {
+                $answer = $app->managers->items->checkRealIsInStock($item);
 
-                $app->container->cache->set(
-                    $app->managers->items->getCacheKeyByEntity($item),
-                    $item->getAttrs()
-                );
+                if (null === $answer) {
+                    $app->container->logger->warning('Unknown item is in stock status', [
+                        'item_id' => $item->getId(),
+                    ]);
+
+//                    if ($vendor = $app->managers->items->getVendor($item)) {
+//                        $vendor->setIsInStockCheck(false);
+//                        $app->managers->vendors->updateOne($vendor);
+//                    }
+                } elseif (false === $answer) {
+                    $item->setIsInStock(false);
+
+                    if ($app->managers->items->updateOne($item)) {
+                        $app->managers->items->indexOne($item);
+                    }
+
+                    $app->container->logger->warning('Item marked as out of stock', [
+                        'item_id' => $item->getId(),
+                    ]);
+                } else {
+                    $app->container->cache->set($checkedCacheKey, true, self::CACHE_LIFETIME_CHECKED);
+                    $app->container->logger->info('Confirmed is in stock item status', [
+                        'item_id' => $item->getId(),
+                    ]);
+                }
             }
+        } else {
+            $answer = false;
+            $app->container->logger->warning('Out of stock item request received', [
+                'item_id' => $id,
+            ]);
         }
 
         $app->response->setJSON(200, [
-//            'in_stock' => true,
-//            'in_stock' => false,
-//            'in_stock' => null,
-            'in_stock' => $answer
+            'in_stock' => $answer,
         ]);
+    }
+
+    private function getCheckedCacheKey(Item $item): string
+    {
+        return str_replace('%id%', $item->getId(), self::CACHE_KEY_CHECKED);
     }
 }
