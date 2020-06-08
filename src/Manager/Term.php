d767c23a825977c1e0e1706b3bd3eb186e60da84
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-06-09 01:46:07 +0300

    +get-value-to-component-id method got back

--- src/Manager/Term.php
+++ src/Manager/Term.php
@@ -3,7 +3,26 @@
 namespace SNOWGIRL_SHOP\Manager;
 
 use SNOWGIRL_CORE\Manager;
+use SNOWGIRL_SHOP\Entity\Term as TermEntity;
+use SNOWGIRL_CORE\Helper\Arrays;
 
 abstract class Term extends Manager
 {
+    public function getValueToComponentId(array $where = null, bool $isLowercase = false): array
+    {
+        $output = [];
+
+        foreach ($this->clear()->setWhere($where)->getObjects() as $term) {
+            /** @var TermEntity $term */
+            $output[$term->getValue()] = $term->getComponentId();
+        }
+
+        if ($isLowercase) {
+            $output = Arrays::mapByKeyMaker($output, function ($key) {
+                return mb_strtolower($key);
+            });
+        }
+
+        return $output;
+    }
 }
