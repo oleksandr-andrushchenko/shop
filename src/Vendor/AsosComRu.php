36f9d47bf042db454bd3db8a6a811cf76ff8c59b
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-08-18 21:48:51 +0300

    +updated +get-item-target-link method added +soft non-func re-factoring

--- src/Vendor/AsosComRu.php
+++ src/Vendor/AsosComRu.php
@@ -2,12 +2,32 @@
 
 namespace SNOWGIRL_SHOP\Vendor;
 
+use SNOWGIRL_SHOP\Entity\Item;
 use SNOWGIRL_SHOP\Vendor;
 
 class AsosComRu extends Vendor
 {
-    public function getBuySelector()
+    public function getBuySelector(): ?string
     {
-        return '.asos-product.pg-in-stock';
+        return '.asos-product .add-item .add-button';
+    }
+
+    public function getItemTargetLink(Item $item): ?string
+    {
+        if (!$partnerLink = $item->getPartnerLink()) {
+            return null;
+        }
+
+        if (!$query = parse_url($partnerLink, PHP_URL_QUERY)) {
+            return null;
+        }
+
+        parse_str($query, $output);
+
+        if (empty($output['ulp'])) {
+            return null;
+        }
+
+        return $output['ulp'];
     }
 }
