8851dd191f63699034da9f6b820bbacfa339d739
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-09-07 01:03:39 +0300

    +soft non-func re-factoring

--- src/Import/Admitad/GroupPrice.php
+++ src/Import/Admitad/GroupPrice.php
@@ -6,7 +6,7 @@ use SNOWGIRL_SHOP\Import\Admitad;
 
 class GroupPrice extends Admitad
 {
-    protected function postNormalizeRow($row)
+    protected function beforeReadRow($row)
     {
         if (isset($row['description'])) {
             $row['description'] = htmlspecialchars_decode($row['description']);
